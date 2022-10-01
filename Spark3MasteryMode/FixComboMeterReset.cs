22e7bf8be05d180e0d7788bdcbd59018b36cfa3e
LandonTownsend <landon.townsend@gmail.com>
2022-10-01 08:39:48 -0700

    fixed combo meter not resetting when it should

---
+++ Spark3MasteryMode/FixComboMeterReset.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace Spark3MasteryMode
+{
+    [HarmonyPatch(typeof(SceneController))]
+    [HarmonyPatch("ResetValues")]
+    class FixComboMeterReset
+    {
+        private static void Postfix()
+        {
+            if (MasteryMod.DifficultyIsMastery())
+            {
+                PlayerHealthAndStats.Combo = 0;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PauseCotrol))]
+    [HarmonyPatch("RestartCheckpoint")]
+    class FixComboMeterResetCheckpoint
+    {
+        private static void Postfix()
+        {
+            if (MasteryMod.DifficultyIsMastery())
+            {
+                PlayerHealthAndStats.Combo = 0;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(HurtControl))]
+    [HarmonyPatch("Death")]
+    class FixComboMeterResetDeath
+    {
+        private static void Postfix()
+        {
+            if (MasteryMod.DifficultyIsMastery())
+            {
+                PlayerHealthAndStats.Combo = 0;
+            }
+        }
+    }
+}
