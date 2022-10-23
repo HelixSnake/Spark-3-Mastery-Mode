0ec9f2b70b1834b053d63f200625edc043ea425d
LandonTownsend <landon.townsend@gmail.com>
2022-10-23 14:41:45 -0700

    Made my best attempt to fix the Extra Effective Magnet Dash bug

--- Spark3MasteryMode/NerfMagnetDash.cs
+++ Spark3MasteryMode/NerfMagnetDash.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
@@ -21,22 +22,6 @@ namespace Spark3MasteryMode
         }
     }
 
-
-    [HarmonyPatch(typeof(Action02_Homing))]
-    [HarmonyPatch("InitialEvents")]
-    class SaveMagnetDashVelocity
-    {
-        public static float minVelocity = 0;
-        static private void Postfix(Action02_Homing __instance, ref PlayerBhysics ___Player)
-        {
-            if (MasteryMod.DifficultyIsMastery())
-            {
-                minVelocity = new Vector3(___Player.rigid.velocity.x, 0f, ___Player.rigid.velocity.z).magnitude + 50f;
-            }
-        }
-    }
-
-
     [HarmonyPatch(typeof(Action02_Homing))]
     [HarmonyPatch("FixedUpdate")]
     class MakeMagnetDashMoreConsistent
@@ -45,20 +30,33 @@ namespace Spark3MasteryMode
         {
             if (MasteryMod.DifficultyIsMastery())
             {
-                if (__instance.Inp.Rewinp.GetButtonDown("Dash") && CharacterAnimatorChange.Character != 2)
+                bool dashAvailable = __instance.Actions.Action00.DashAvailable;
+                if (__instance.Inp.Rewinp.GetButtonDown("Dash") && !dashAvailable && CharacterAnimatorChange.Character != 2)
                 {
                     if (__instance.Target)
                     {
                         ___direction = __instance.Target.position - __instance.transform.position;
                     }
                     ___direction = ___direction.normalized;
-                    if (__instance.Actions.Action00.DashAvailable)
+                    ___Speed = Mathf.Max(___Speed, __instance.InitialHorizontalVelocity + 50);
+                    ___Player.rigid.velocity = ___direction * ___Speed;
+                    __instance.Actions.ChangeAction(1);
+                    return false;
+                }
+                bool dashInput1 = __instance.Inp.Rewinp.GetAxis("Dash") > 0.9f;
+                bool dashInput2 = (int)typeof(Action00_Regular).GetField("DashInputCounter", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .GetValue(__instance.Actions.Action00) == 0;
+                if (dashInput1 && dashInput2 && dashAvailable && __instance.Actions.Action00.dashc)
+                {
+                    if (__instance.Target)
                     {
-                        Vector3 newDir = new Vector3(___direction.x, 0.001f, ___direction.z);
-                        ___Speed *= newDir.magnitude;
-                        ___direction = newDir.normalized;;
+                        ___direction = __instance.Target.position - __instance.transform.position;
                     }
-                    ___Speed = Mathf.Max(___Speed, SaveMagnetDashVelocity.minVelocity);
+                    ___direction = ___direction.normalized;
+                    Vector3 newDir = new Vector3(___direction.x, 0.001f, ___direction.z);
+                    ___Speed *= newDir.magnitude;
+                    ___direction = newDir.normalized; ;
+                    ___Speed = Mathf.Max(___Speed, __instance.InitialHorizontalVelocity + 50);
                     ___Player.rigid.velocity = ___direction * ___Speed;
                     __instance.Actions.ChangeAction(1);
                     return false;
