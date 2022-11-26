bd33a12af5ab38b3e17fcdedbf7dcf9599972bfa
LandonTownsend <landon.townsend@gmail.com>
2022-11-26 07:31:12 -0800

    Fixed throwback boss fight softlock

---
+++ Spark3MasteryMode/FixHystoriaBossSoftLock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HelixBugFix
+{
+	[HarmonyPatch(typeof(HystoriaBossFight))]
+	[HarmonyPatch("FixedUpdate")]
+	class FixHystoriaBossSoftLock
+	{
+		private static void Postfix(HystoriaBossFight __instance, ref float ___c, ref float ___c2, ref int ___boss)
+		{
+			if (HelixFixMod.DifficultyIsNotMastery())
+			{
+				if (___c > __instance.FirstPhaseBossFightDuration && ___c2 > __instance.TransitionTime)
+				{
+					if (__instance.Ryno.HP <= 0f && __instance.Double.HP <= 0f && ___boss == 1) // FixedUpdate is 1 run into the softlock loop
+					{
+						___boss = 0; // break out of softlock; set boss to Double so it will transition to EJ correctly
+					}
+				}
+			}
+		}
+	}
+}
