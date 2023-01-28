b14c88f90d85d88ed15a70f1b428551e30dcbdd4
LandonTownsend <landon.townsend@gmail.com>
2023-01-28 08:17:09 -0800

    Removed redundent pulley fix and added down dash fix

---
+++ Spark3MasteryMode/FixDownDashLoss.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace HelixBugFix
+{
+    [HarmonyPatch(typeof(Action08_SuperMoves))]
+    [HarmonyPatch("SuperManagement")]
+    class FixDownDashLoss
+    {
+        private static void Prefix(Action08_SuperMoves __instance, ref float ___count, ref bool ___DownDashAvailable)
+        {
+            if (HelixFixMod.DifficultyIsNotMastery())
+            {
+
+                if (__instance.AttackType >= 0)
+                {
+                    ___count += PlayerBhysics.TimeStep;
+                    if (___count > 3)
+                    {
+                        ___DownDashAvailable = true;
+                    }
+                }
+            }
+        }
+    }
+}
