b0cb9d6771a7dd6774b305b592509774aeed7724
LandonTownsend <landon.townsend@gmail.com>
2023-07-17 14:49:15 -0700

    Fix index

--- Spark3MasteryMode/ResetExploreMedals.cs
+++ Spark3MasteryMode/ResetExploreMedals.cs
@@ -16,16 +16,16 @@ namespace Spark3ResetExploreMedalsMod
     {
         private static void Prefix()
         {
-            Save.GetCurrentSave().ExploreMedal0[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal1[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal2[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal3[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal4[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal5[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal6[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal7[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal8[SceneController.LastLoadedLevel] = false;
-            Save.GetCurrentSave().ExploreMedal9[SceneController.LastLoadedLevel] = false;
+            Save.GetCurrentSave().ExploreMedal0[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal1[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal2[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal3[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal4[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal5[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal6[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal7[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal8[Save.CurrentStageIndex] = false;
+            Save.GetCurrentSave().ExploreMedal9[Save.CurrentStageIndex] = false;
         }
         private static void Postfix()
         {
